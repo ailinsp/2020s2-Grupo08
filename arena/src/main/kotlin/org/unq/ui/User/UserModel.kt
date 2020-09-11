188366319a233b4e287ae858c8d1ae6bbd472081
Fsardi96 <facusardi96@gmail.com>
2020-09-11 19:41:46 -0300

    Se crea tabla

--- arena/src/main/kotlin/org/unq/ui/User/UserModel.kt
+++ arena/src/main/kotlin/org/unq/ui/User/UserModel.kt
@@ -2,13 +2,23 @@ package org.unq.ui.User
 
 import org.unq.ui.bootstrap.getInstagramSystem
 import org.unq.ui.model.InstagramSystem
+import org.unq.ui.model.Post
 import org.uqbar.commons.model.annotations.Observable
 
 @Observable
 class UserModel(val instagramSystem: InstagramSystem = getInstagramSystem()) {
 
-    fun search(msg: String) {
-        instagramSystem.searchByTag(msg)
+    var post = emptyList<Post>()
+    var selected = ""
+
+    fun searchDescription(tag: String): List<Post> {
+       return instagramSystem.searchByTag(tag)
     }
+
+    fun getPostByUser(userId: String){
+         post = instagramSystem.searchByUserId(userId)
+    }
+
+
 
 }
--- arena/src/main/kotlin/org/unq/ui/User/UserView.kt
+++ arena/src/main/kotlin/org/unq/ui/User/UserView.kt
@@ -1,6 +1,7 @@
 package org.unq.ui.User
 
 
+import org.uqbar.arena.kotlin.extensions.*
 import org.uqbar.arena.widgets.Panel
 import org.uqbar.arena.windows.SimpleWindow
 import org.uqbar.arena.windows.WindowOwner
@@ -8,9 +9,49 @@ import org.uqbar.arena.windows.WindowOwner
 class UserView(owner: WindowOwner, model: UserModel): SimpleWindow<UserModel>(owner, model){
     override fun addActions(p0: Panel?) {}
 
-    override fun createFormPanel(p0: Panel?) {
+    override fun createFormPanel(mainPanel: Panel) {
         title = "Post de User"
         setMinWidth(300)
+
+        table<UserModel>(mainPanel) {
+            bindItemsTo("post")
+            bindSelectionTo("selected")
+            visibleRows = 10
+
+
+            column {
+                title = "#"
+                fixedSize = 30
+                bindContentsTo("id")
+            }
+            column {
+                title = "LandScape"
+                fixedSize = 130
+                bindContentsTo("landscape")
+            }
+            column {
+                title = "Portrait"
+                fixedSize = 230
+                bindContentsTo("portrait")
+            }
+        }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
