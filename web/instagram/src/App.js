a9600fb33149943e95c74b20b07d8a18cf892105
Ailin Patiño <ailinsp50@gmail.com>
2020-11-06 23:30:00 -0300

    Agrego pantalla login

--- web/instagram/src/App.js
+++ web/instagram/src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch } from "react-router-dom";
 import './App.css';
+import Login from './components/Login/login';
 import Header from './components/Header/header';
+
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
+      <Router>
+      <Header />
+      <div className="container">
+        <Switch>
+          <Login />
+
+        </Switch>
       </div>
+    </Router>
     );
   }
 }
---
+++ web/instagram/src/components/Login/login.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+import "./Login.css";
+
+const Login = () => {
+  const history = useHistory();
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    setData({
+      ...data,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3000/login", data)
+      .then((response) => {
+        localStorage.setItem("token", response.headers.authorization);
+        localStorage.setItem("userData", JSON.stringify(response.data));
+        history.push("/home");
+      })
+      .catch((error) => console.log("Error: ", error));
+  };
+
+  return (
+    <div class="container-fluid">
+        <div class="row no-gutter">
+            <div class="d-none d-md-flex col-md-4 col-lg-6 bg-image"></div>
+                <div class="col-md-8 col-lg-6">
+                    <div class="login d-flex align-items-center py-5">
+                         <div class="container">
+                             <div class="row">
+                                <div class="col-md-9 col-lg-8 mx-auto">
+                                    <h3 class="login-heading mb-4">Instagram</h3>
+                                    <form>
+                                        <div class="form-label-group">
+                                            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus></input>
+                                            <label for="inputEmail">Email</label>
+                                        </div>
+
+                                        <div class="form-label-group">
+                                            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required></input>
+                                            <label for="inputPassword">Password</label>
+                                        </div>
+                                        <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit">Login</button>
+                                        <button class="btn btn-lg btn-primary btn-block btn-login text-uppercase font-weight-bold mb-2" type="submit">Register</button>
+                                     </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+  );
+
+};
+
+export default Login;
