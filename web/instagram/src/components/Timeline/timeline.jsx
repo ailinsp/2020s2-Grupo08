a08279989b009a67484b2018ab761142670effec
Ailin Patiño <ailinsp50@gmail.com>
2020-11-12 19:29:03 -0300

    update timeline

--- web/instagram/src/components/Timeline/timeline.jsx
+++ web/instagram/src/components/Timeline/timeline.jsx
@@ -1,40 +1,63 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import axios from "axios";
 
-function Posts() {
-  const [posts, setPosts] = useState([]);
-
- 
-
-    useEffect(() => {
-        axios.get(`http://localhost:7000/user`) 
-            .then(res => setPosts(res.data))
-            .catch(err => console.error(err))
-    }, []);
-
-    return (
-        <div style={{width: '90vw'}}>
-            <div> Timeline </div>
-            <div>
-                {posts.map((post, indx) => (
-                    post(indx, post)
-                ))
-                }
-            </div>
-        </div>
-    )
-
-    function post(indx, post) {
-        return <div title={post.description}> </div>;
-    }
+//contiene name, image, followers, timeline
+const getLoggedUser = () => {
+    return axios.get('http://localhost:7000/user')
+      .then(response => response.data)
+      .catch(error => Promise.reject(error.response.data))
 }
 
-function Timeline() {
-  return(
-    <Posts/>
-  )
+const getTimeline = () => {
+    //getLoggedUser().???
+}
+
+const getPosts = () => {
+    //getTimeline().????
+}
+
+const Post = ({ data }) => {
+    const { id, description, portrait, landscape, likes, date, user } = data;
   
-
+    return (
+        <div>
+            <img src={landscape} alt={user} />
+        </div>
+    );
 }
+
+class Timeline extends React.Component {
+
+    constructor(props) {
+      super(props)
+      this.state = {
+        posts: []
+      }
+    }
+
+    componentDidMount() {
+        getPosts()
+          .then(posts => this.setState({ posts }))
+          .catch(error => this.setState({ error }))
+    }
+  
+    renderPosts() {
+        const { posts } = this.state;
+        
+        return (
+            <div>
+                {posts.map(post => <Post data={post} />)}
+            </div>
+        );
+      }
+    
+    render() {
+        return (
+            <div className="container">
+                {this.renderPosts()}
+            </div>
+        );
+      }
+    }
 
 export default Timeline;
