da9cd281de741d2da4a6d4bb0d2a530fe2c4f808
Ailin Patiño <ailinsp50@gmail.com>
2020-11-11 21:52:41 -0300

    agrego auth?

--- web/instagram/src/components/Timeline/timeline.jsx
+++ web/instagram/src/components/Timeline/timeline.jsx
@@ -1,45 +1,42 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
-import Post from './components/Post/post.jsx';
 
-class Timeline extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [],
-    }
-  }
+function Posts() {
+  const [posts, setPosts] = useState([]);
 
-  componentDidMount(){
-      axios
-        .get("http://localhost:7000/user")   
-        .then(response => response.data).then(posts => this.setState({ posts }))
-  }
+    useEffect(() => {
+        axios.get(`http://localhost:7000/user`, 
+          {
+          headers: {
+            'Authorization': `token ${access_token}`
+          }
+            .then(res => setPosts(res.data))
+            .catch(err => console.error(err))
+    }, []);
 
-  renderPosts() {
-    const { posts } = this.state;
-    if(posts.length === 0) {
-      return <div>Loading!!</div>
-    }
     return (
-      <div>
-        {posts.map(post => <Post data={post} />)}
-      </div>
-    );
-  }
+        <div style={{width: '90vw'}}>
+            <div> Timeline </div>
+            <div>
+                {posts.map((post, indx) => (
+                    post(indx, post)
+                ))
+                }
+            </div>
+        </div>
+    )
 
-  render() {
-    const { name } = this.user || {};
-    
-    return (
-      <div className="container">
-        <h1>Hello {name}!!</h1>
-        {this.renderPosts()}
-      </div>
-    );
-  }
+    function post(indx, post) {
+        return <div title={post.description}> </div>;
+    }
+}
+
+function Timeline() {
+  return(
+    <Posts/>
+  )
+  
+
 }
 
 export default Timeline;
-
-
